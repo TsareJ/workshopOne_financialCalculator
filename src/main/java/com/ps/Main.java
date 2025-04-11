6401bd5e58f179b1b7e77071425df3b54c7e357b
TsareJ <tsarejoseph@gmail.com>
2025-04-11 14:53:55 -0400

    Laid out variables

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -5,6 +5,9 @@ public class Main {
         System.out.println("Hello, World!");
     }
         Scanner scanner = new Scanner(System.in);
-
-
+        System.out.println("Arise thy Financial Calculator!!");
+        System.out.println("Choose my form below:");
+        System.out.println("1) Mortgage Calculator");
+        System.out.println("2) Vehicle Appreciation Calculator");
+        System.out.println("3) Getting Old Fund Calculator");
 }
