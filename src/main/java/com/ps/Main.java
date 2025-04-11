1ab982f4904da583c031a89afc25dee594157e13
TsareJ <tsarejoseph@gmail.com>
2025-04-11 15:25:58 -0400

    added menu

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,13 +1,30 @@
 package com.ps;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
         Scanner scanner = new Scanner(System.in);
         System.out.println("Arise thy Financial Calculator!!");
         System.out.println("Choose my form below:");
         System.out.println("1) Mortgage Calculator");
         System.out.println("2) Vehicle Appreciation Calculator");
         System.out.println("3) Getting Old Fund Calculator");
+        System.out.println("Select from my form (1-3): ");
+        int givenCommand = scanner.nextInt();
+
+        switch (givenCommand) {
+
+            case 1:
+                //Mortgage Calculator
+                System.out.println("The Mortgage Calculator has risen!")
+                System.out.println("Enter principle loan amount: ");
+                System.out.println("Enter interest rate: ");
+                System.out.println("Enter loan term in years: ");
+            default:
+
+
+        }
+    }
 }
+
