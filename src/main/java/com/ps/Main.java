ad426927f0e9b708f1fe0af36039181d27538123
TsareJ <tsarejoseph@gmail.com>
2025-04-12 10:45:54 -0400

    cleaned up code and input formulas better

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,6 +1,5 @@
 package com.ps;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 import static java.lang.System.*;
@@ -8,25 +7,33 @@ import static java.lang.System.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(in);
-        out.println("Arise thy Financial Calculator!!");
-        out.println("Choose my form below:");
-        for (String s : Arrays.asList("1) Mortgage Calculator", "2) Vehicle Appreciation Calculator", "3) Getting Old Fund Calculator", "Select from my form (1-3): ")) {
-            out.println(s);
-        }
-        int givenCommand = scanner.nextInt(1);
+        System.out.println("Arise thy Financial Calculator!!");
+        System.out.println("Choose my form below:");
+        out.println("1) Mortgage Calculator");
+        out.println("2) Vehicle Appreciation Calculator");
+        out.println("3) Getting Old Fund Calculator");
+        out.println("Select from my form (1-3): ");
+        int givenCommand = scanner.nextInt();
 
         switch(givenCommand) {
 
             case 1:
                 //Mortgage Calculator
-                out.println("The Mortgage Calculator has risen!");
-                out.println("Enter principal loan amount ($): ");
-                out.println("Enter interest annually rate: ");
-                out.println("Enter loan term in years: ");
+                System.out.println("The Mortgage Calculator has risen!");
+                System.out.println("Enter principal loan amount ($): ");
+                System.out.println("Enter interest annually rate: ");
+                System.out.println("Enter loan term in years: ");
 
                 float principal = scanner.nextFloat();
                 double interestRateAnnually = scanner.nextDouble();
-                short loanTerm = scanner.nextByte();
+                short loanTerm = scanner.nextShort();
+
+                double monthlyRate = (interestRateAnnually / 100 / 12);
+                int numPayments = loanTerm * 12;
+
+                double factor = Math.pow(1 + monthlyRate,numPayments);
+                double monthlyPayment = principal * (monthlyRate *factor) / (factor-1);
+                double totalInterest = (monthlyPayment * numPayments) - principal;
 
 
                 break;
