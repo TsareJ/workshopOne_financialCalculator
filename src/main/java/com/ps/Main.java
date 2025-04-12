99a5f67fbf50150d946d9555c19c142cbb510c1c
TsareJ <tsarejoseph@gmail.com>
2025-04-12 09:57:30 -0400

    tried to do something cool to make my code standout and it wouldnt run ended up raing and deleting everything

--- src/main/java/com/ps/Main.java
+++ src/main/java/com/ps/Main.java
@@ -1,39 +1,33 @@
 package com.ps;
 
+import java.util.Arrays;
 import java.util.Scanner;
+
+import static java.lang.System.*;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Arise thy Financial Calculator!!");
-        System.out.println("Choose my form below:");
-        System.out.println("1) Mortgage Calculator");
-        System.out.println("2) Vehicle Appreciation Calculator");
-        System.out.println("3) Getting Old Fund Calculator");
-        System.out.println("Select from my form (1-3): 1");
-        int givenCommand = scanner.nextInt();
+        Scanner scanner = new Scanner(in);
+        out.println("Arise thy Financial Calculator!!");
+        out.println("Choose my form below:");
+        for (String s : Arrays.asList("1) Mortgage Calculator", "2) Vehicle Appreciation Calculator", "3) Getting Old Fund Calculator", "Select from my form (1-3): ")) {
+            out.println(s);
+        }
+        int givenCommand = scanner.nextInt(1);
 
         switch(givenCommand) {
 
             case 1:
                 //Mortgage Calculator
-                System.out.println("The Mortgage Calculator has risen!");
-                System.out.println("Enter principal loan amount ($): 53000");
-                System.out.println("Enter interest annually rate: 7.625");
-                System.out.println("Enter loan term in years: 15 ");
+                out.println("The Mortgage Calculator has risen!");
+                out.println("Enter principal loan amount ($): ");
+                out.println("Enter interest annually rate: ");
+                out.println("Enter loan term in years: ");
 
                 float principal = scanner.nextFloat();
                 double interestRateAnnually = scanner.nextDouble();
-                byte loanTerm = scanner.nextByte();
+                short loanTerm = scanner.nextByte();
 
-                double monthlyRate = (interestRateAnnually / 100) /12;
-                int numPayments = loanTerm * 12;
-
-                double factor = Math.multiplyExact((long) monthlyRate, numPayments);
-                double monthlyPayment = principal * (monthlyRate * factor) / (factor-1);
-                double totalInterest = monthlyPayment * numPayments - principal;
-
-                System.out.printf("")
 
                 break;
 
@@ -52,7 +46,7 @@ public class Main {
                 break;
 
             default:
-                System.out.println("Form not selected");
+                out.println("Form not selected");
 
 
         }
